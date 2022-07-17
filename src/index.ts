52fd2799e654fc0826aae95833f1512e1644b979
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-07-17 13:13:07 -0400

    renamed file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import { GameConfig } from './config';
-import { DemoScene } from './scenes/demoScene';
+import { DemoScene } from './scenes/demo';
 
 new Phaser.Game(
   Object.assign(GameConfig, {
